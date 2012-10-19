368aba80b0b0fcbd41fbbe15e3dda20e9c8b8996
davidnuon <david@davidnuon.com>
2012-10-18 17:08:51 -0700

    Changed namespace of Item and Post

--- app/model/post.php
+++ app/model/post.php
@@ -1,21 +1,52 @@
 <?php
 
-namespace Model;
+namespace Model\Basic;
 
-class Item {
-	private $title = '';
-	private $date = '';
+class ExtraContent {}
+
+class Item 
+{
+	protected $title = '';
+	protected $date = '';
 	
-	function __construct($title, $date) {
+	function __construct($title, $date)
+	{
 		$this->title = $title;
 		$this->date  = $date;
 	}
 
-	function getTitle() {
+	public function getTitle()
+	{
 		return $this->title;
 	}
 
-	function getDate() {
+	public function getDate() 
+	{
 		return $this->date;
+	}
+}
+
+class Post extends Item
+{
+	protected $content = '';
+	protected $extraContent = array();
+
+	function __construct($title, $date, $content)
+	{
+		$this->content = $content;
+		parent::__construct($title, $date);
+	}
+
+	public function getContent() 
+	{
+		return $this->content;
+	}
+
+	public function addExtraContent($name, $extraContent) 
+	{
+		if($extraContent instanceof ExtraContent)
+		{
+			$this->extraContent[$name] = $extraContent;
+		}
 	}
 }
