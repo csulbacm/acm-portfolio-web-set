00fee65010b2208a1e241e0e32a7686aec07e63d
davidnuon <david@davidnuon.com>
2013-03-04 09:48:06 -0800

    Started repsonsive

--- app/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include_once(dirname(__FILE__) . "\util\base\JsonMap.php");
-	include_once(dirname(__FILE__) . "\model\Post.php");
-	include_once(dirname(__FILE__) . "\model\Container.php");
-	include_once(dirname(__FILE__) . "\util\FileList.php");
-	include_once(dirname(__FILE__) . "\util\Router.php");
-	include_once(dirname(__FILE__) . "\util\Config.php");
-?>
--- tests/ut/ut_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Tester\Tester as Tester;
-
-$ut_Tester = new Tester("Testing"); 
-$ut_Tester->pass('it works!');
-$ut_Tester->fail('This should fail.');
-
-?>
